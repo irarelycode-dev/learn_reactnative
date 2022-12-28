20122922b27619c73b96c1138036004a3d1c3b0e
vicky <irarelycode@gmail.com>
2022-12-28 13:08:05 -0500

    firebase authentication config added

---
+++ functions/create_user.js
@@ -0,0 +1,18 @@
+const admin = require("firebase-admin");
+
+module.exports = function (req, res) {
+  //verify if the user provides a phone number
+  if (!req.body.phone) {
+    res.status(422).send({ error: "Bad input" });
+  }
+
+  //format the phone number to remove the dashes
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
+
+  //create a new user account using that phone number
+  admin
+    .auth()
+    .createUser({ uid: phone })
+    .then((user) => res.send(user))
+    .catch((err) => res.status(422).send({ error: err }));
+};
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,19 @@
+const admin = require("firebase-admin");
 const functions = require("firebase-functions");
+const createUser = require("./create_user");
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //
+
+const serviceAccount = require("./one-time-password-8a4e1-firebase-adminsdk-jtqnl-2c160cac20.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL:
+    "https://one-time-password-8a4e1-default-rtdb.asia-southeast1.firebasedatabase.app",
+});
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
@@ -11,3 +22,5 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 exports.goodBye = functions.https.onRequest((request, response) => {
   response.send("goodbye function");
 });
+
+exports.createUser = functions.https.onRequest(createUser);
