176d9968f87ddffbc462b6ae024dcdff3d0bae02
vicky <irarelycode@gmail.com>
2023-01-01 15:23:27 -0500

    signin form added

--- functions/verify_otp.js
+++ functions/verify_otp.js
@@ -2,12 +2,18 @@ const admin = require("firebase-admin");
 const { ref } = require("firebase-functions/v1/database");
 
 module.exports = function (req, res) {
-  res.set("Access-Control-Allow-Origin", "*");
+  console.log("flag1", req.body);
+
   if (!req.body.phone || !req.body.code) {
     return res.status(422).send({ error: "Phone and code must be provided" });
   }
+
+  console.log("flag2");
+
   const phone = String(req.body.phone).replace(/[^\d]/g, "");
   const code = parseInt(req.body.code);
+
+  console.log("flag3 ", code);
 
   admin
     .auth()
@@ -17,17 +23,22 @@ module.exports = function (req, res) {
         .database()
         .ref("users/" + phone)
         .on("value", (snapshot) => {
-          ref.off();
           const user = snapshot.val();
+          console.log("user", user);
           if (user.code != code || !user.codeValid) {
             return res.status(422).send({ error: "Code not valid" });
           }
-          ref.update({ codeValid: false });
           admin
-            .auth()
-            .createCustomToken(phone)
-            .then((token) => {
-              res.send({ token });
+            .database()
+            .ref("users/" + phone)
+            .update({ codeValid: false })
+            .then(() => {
+              admin
+                .auth()
+                .createCustomToken(phone)
+                .then((token) => {
+                  return res.send({ token });
+                });
             });
         });
     })
